1e97a2b43c2ff399c58b10a0e0695fb8fbe51652
noguerajulioces <noguerajulio24@gmail.com>
2023-11-24 17:31:11 -0300

    move to custom drawer

---
+++ lib/src/app/custom_drawer.dart
@@ -0,0 +1,58 @@
+// custom_drawer.dart
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  final Function(int) onItemSelected;
+
+  const CustomDrawer({
+    Key? key,
+    required this.onItemSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      // Aquí construyes tu Drawer con todos los elementos que necesitas
+      child: ListView(
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          ListTile(
+            title: Text('Quitar Anuncios'),
+            leading: Icon(Icons.gif),
+            onTap: () => onItemSelected(0),
+          ),
+          ListTile(
+            title: Text('Idiomas'),
+            leading: Icon(Icons.language),
+            onTap: () => onItemSelected(0),
+          ),
+          ListTile(
+            title: Text('Comparte y Gana'),
+            leading: Icon(Icons.card_giftcard),
+            onTap: () => onItemSelected(0),
+          ),
+          ListTile(
+            title: Text('Comentarios'),
+            leading: Icon(Icons.message),
+            onTap: () => onItemSelected(0),
+          ),
+          ListTile(
+            title: Text('Califícanos'),
+            leading: Icon(Icons.star),
+            onTap: () => onItemSelected(0),
+          ),
+          Spacer(),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Divider(),
+                Text('Versión de la app 0.1.0'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/src/screens/main_container_screen.dart
+++ lib/src/screens/main_container_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import '../screens/loans_screen.dart'; // Importa las pantallas aquí
+import '../screens/loans_screen.dart';
 import '../screens/home_screen.dart';
 import '../screens/donate_screen.dart';
 import '../app/bottom_navigation_bar.dart';
-import '../app/app_bar.dart'; // Asegúrate de que la ruta de importación sea correcta
+import '../app/app_bar.dart';
+import '../app/custom_drawer.dart';
 
 class MainContainerScreen extends StatefulWidget {
   @override
@@ -36,73 +37,15 @@ class _MainContainerScreenState extends State<MainContainerScreen> {
         index: _currentIndex,
         children: _screens,
       ), // Muestra la pantalla seleccionada sin perder el estado
-      drawer: Drawer(
-        // Aquí construyes tu Drawer con todos los elementos que necesitas
-        child: ListView(
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            ListTile(
-              title: Text('Quitar Anuncios'),
-              leading: Icon(Icons.gif),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 0;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Idiomas'),
-              leading: Icon(Icons.language),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 0;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Comparte y Gana'),
-              leading: Icon(Icons.card_giftcard),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 0;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Comentarios'),
-              leading: Icon(Icons.message),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 1;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Califícanos'),
-              leading: Icon(Icons.star),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 2;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            Spacer(),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Divider(),
-                  Text('Versión de la app 0.1.0'),
-                ],
-              ),
-            ),
-          ],
-        ),
+      drawer: CustomDrawer(
+        onItemSelected: (index) {
+          // Aquí puedes manejar la lógica de selección de ítems del drawer
+          // Por ejemplo, cambiar el índice actual o cerrar el drawer
+          setState(() {
+            _currentIndex = index;
+          });
+          Navigator.pop(context); // Cierra el Drawer
+        },
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: _currentIndex,
