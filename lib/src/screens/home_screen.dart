05196fcb2fc15b8dd811f8b79196129b817bbb39
noguerajulioces <noguerajulio24@gmail.com>
2023-11-25 23:51:10 -0300

    add localcurrency

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
+
+String getLocalCurrencySymbol() {
+  var format = NumberFormat.simpleCurrency(locale: Intl.systemLocale);
+  return format.currencySymbol;
+}
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    String currencySymbol = getLocalCurrencySymbol();
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -15,7 +23,7 @@ class HomeScreen extends StatelessWidget {
             buildInputField(
               context,
               AppLocalizations.of(context)!.loanAmount,
-              'GS. ',
+              currencySymbol,
               Icons.monetization_on,
             ),
             const SizedBox(
