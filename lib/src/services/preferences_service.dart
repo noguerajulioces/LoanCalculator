02b668e6d1c82f01e10c86e5204d3911e9504660
noguerajulioces <noguerajulio24@gmail.com>
2023-11-25 22:49:23 -0300

    change for get the app language

--- lib/src/services/preferences_service.dart
+++ lib/src/services/preferences_service.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Future<void> saveLanguage(String languageCode) async {
   final prefs = await SharedPreferences.getInstance();
@@ -7,5 +9,22 @@ Future<void> saveLanguage(String languageCode) async {
 
 Future<String> getSavedLanguage() async {
   final prefs = await SharedPreferences.getInstance();
-  return prefs.getString('selectedLanguage') ?? 'en';
+  String? savedLanguageCode = prefs.getString('selectedLanguage');
+
+  if (savedLanguageCode != null) {
+    return savedLanguageCode;
+  }
+
+  String systemLanguageCode =
+      WidgetsBinding.instance!.window.locale.languageCode;
+
+  List<String> supportedLanguages = AppLocalizations.supportedLocales
+      .map((locale) => locale.languageCode)
+      .toList();
+
+  if (supportedLanguages.contains(systemLanguageCode)) {
+    return systemLanguageCode;
+  }
+
+  return 'en';
 }
